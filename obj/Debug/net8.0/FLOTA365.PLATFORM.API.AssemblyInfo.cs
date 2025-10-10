873c5940b01fa8c9b6c0d2ca3a3a5694e360935d
jlinares <jorgelinaresrrs301@gmail.com>
2025-10-10 09:43:57 -0500

    debug update

--- obj/Debug/net8.0/FLOTA365.PLATFORM.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/FLOTA365.PLATFORM.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Flota365.Platform.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c7b177342392b6f74319760dc01b7280ff8f419")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d24143819080f35e575a4e4fa75062b266a201a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Flota365.Platform.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Flota365.Platform.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
